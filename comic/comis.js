e693773971582351dc347684ff8bef77f40d4bb9
draperkristine <draperkristine12@gmail.com>
2017-10-30 13:21:17 -0400

    Add files via upload

---
+++ comic/comis.js
@@ -0,0 +1,34 @@
+var Answer;
+var Answer2;
+var img1 = spongehappy.jpg;
+var img2 = spongecop.jpg;
+var img3 = Tweety_Bird.gif;
+var img4 = bettyboo.jpg
+img5 = popeye.jpg;
+if(Answer == Secret){
+
+	console.log(true);
+} 
+else{
+	console.log(try again);
+}
+
+if(Answer2 == EMPTY){
+	console.log(true);
+}
+else{
+	console.log(try again);
+}
+
+if(Answer,Answer2 == true){
+
+var popeye = {
+
+var converse = prompt ("hey Tweety" + img3 + "Hello popeye" + "Have you seen betty boo she was 
+	to meet me but she never showed " + img5 + "no haven't seen her"  + "you need to be careful tweety" +
+	img3 +"why" + img5 + "spongecop is in one of his moods" + img2 + "hey no standing around espically you tweety hate birds always pooping on things" + "get out of my sight before I call tom tweety" +
+img3 + "well excuse me what got his holes all sour" + img5 + "his lady friend just left him" + img3 + "who?" +
+img5 + "bettyboo" + img4 + "bo bo dee doo")
+
+}
+}
